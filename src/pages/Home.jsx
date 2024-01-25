09ab1b8256c70fb9f57cd18c0b7e71f3bd10d9bc
Riazul Islam <riazul5207@gmail.com>
2024-01-25 10:11:15 +0600

    card ui completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
 import './home.css';
+import Card from '../components/Card';
+import Img from '../assets/images/nano-sites.png';
 
 const Home = () => {
     return (
         <div className="home">
             <Header/>
-            <div className="projects"></div>
+
+            <div className="projects">
+                <Card thumb={Img} title={"Project Title"} path={'/test'}/>
+                <Card thumb={Img} title={"Project Title"} path={'/test'}/>
+                <Card thumb={Img} title={"Project Title"} path={'/test'}/>
+                <Card thumb={Img} title={"Project Title"} path={'/test'}/>
+                <Card thumb={Img} title={"Project Title"} path={'/test'}/>
+            </div>
         </div>
     );
 }
