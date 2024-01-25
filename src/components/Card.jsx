b60f6719a33bb9234b05e0ad63d7093f9013c5a1
Riazul Islam <riazul5207@gmail.com>
2024-01-25 09:49:35 +0600

    project card created

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './card.css';
+
+const Card = ({ thumb, title, path }) => {
+    return (
+        <div className="card">
+            <Link to={path} target='_blank'>
+                <div className="thumb">
+                    <img src={thumb} alt="project-thumb" />
+                    <div className="tags">
+                        <span className="react">react</span>
+                        <span className="css">css</span>
+                    </div>
+                </div>
+            </Link>
+            <div className="title">
+                <p>{title}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
