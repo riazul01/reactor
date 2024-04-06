f927f1c3cb481f3a24e5fb57e98627ae62ecdf0b
Riazul Islam <riazul5207@gmail.com>
2024-04-06 16:40:34 +0600

    project link issue fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
             {/* https://googlefonts.github.io/noto-emoji-animation/ */}
             <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f6a9/512.gif" alt="🚩" width="42" height="42"/>
             <h1>Project List</h1>
-            <Link to="/clock" className="projectLink">01. Clock</Link>
-            <Link to="/stopwatch" className="projectLink">02. Stopwatch</Link>
-            <Link to="/recursive-partitioning" className="projectLink">03. Recursive Partitioning</Link>
+            <Link to="/clock" className="projectLink">01. Clock</Link><br/>
+            <Link to="/stopwatch" className="projectLink">02. Stopwatch</Link><br/>
+            <Link to="/recursive-partitioning" className="projectLink">03. Recursive Partitioning</Link><br/>
             <p className="footer">Created by <a href="https://github.com/riazul01" className="gitHubLink" target="_blank" rel="noreferrer">riazul01</a></p>
         </div>
     );
