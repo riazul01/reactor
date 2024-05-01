e9971406f085cc8eab3805b8362308488b8f9b00
Riazul Islam <riazul5207@gmail.com>
2024-05-01 12:06:46 +0600

    stopwatch project updated

--- src/pages/projects/Stopwatch.jsx
+++ src/pages/projects/Stopwatch.jsx
@@ -34,7 +34,7 @@ const Stopwatch = () => {
     }
 
     return (
-        <div className="stopwatch-bg h-[100vh] w-full flex items-center justify-center" style={{fontFamily: 'Rethink Sans, sans-serif'}}>
+        <div className="h-[100vh] w-full flex items-center justify-center bg-[#1e2b3b]" style={{fontFamily: 'Rethink Sans, sans-serif'}}>
             <div className="mx-[0.6rem] my-0 p-[1rem] w-auto h-auto flex flex-col items-center justify-center bg-[#000] bg-opacity-50 rounded-[2rem]">
                 <div className="px-0 py-[1.2rem] flex flex-wrap items-center justify-center">
                     <p className="m-[0.2rem] w-[120px] text-[#fff] text-[3rem] text-center font-[500] rounded-[0.2rem]">
@@ -55,12 +55,12 @@ const Stopwatch = () => {
                     </p>
                 </div>
                 <div className="pt-[0.6rem] pb-[1rem] flex flex-wrap items-center justify-center">
-                    <button onClick={startTimer} disabled={controls.start} className={`m-[0.4rem] px-[1rem] py-[0.4rem] text-[1.2rem] font-[500] border-none outline-none cursor-pointer rounded-[0.2rem] transition-all ease-in-out duration-300 ${controls.start ? 'opacity-30' : 'opacity-100'} bg-[seagreen]`} style={{fontFamily: 'Rethink Sans, sans-serif'}}>Start</button>
-                    <button onClick={pauseTimer} disabled={controls.pause} className={`m-[0.4rem] px-[1rem] py-[0.4rem] text-[1.2rem] font-[500] border-none outline-none cursor-pointer rounded-[0.2rem] transition-all ease-in-out duration-300 ${controls.pause ? 'opacity-30' : 'opacity-100'} bg-[crimson]`} style={{fontFamily: 'Rethink Sans, sans-serif'}}>Pause</button>
-                    <button onClick={resetTimer} disabled={controls.reset} className={`m-[0.4rem] px-[1rem] py-[0.4rem] text-[1.2rem] font-[500] border-none outline-none cursor-pointer rounded-[0.2rem] transition-all ease-in-out duration-300 ${controls.reset ? 'opacity-30' : 'opacity-100'} bg-[steelblue]`} style={{fontFamily: 'Rethink Sans, sans-serif'}}>Reset</button>
+                    <button onClick={startTimer} disabled={controls.start} className={`m-[0.4rem] px-[1rem] py-[0.4rem] text-[1.2rem] font-[500] border-none outline-none cursor-pointer rounded-[0.2rem] transition-all ease-in-out duration-300 ${controls.start ? 'opacity-40' : 'opacity-100'} bg-[seagreen]`} style={{fontFamily: 'Rethink Sans, sans-serif'}}>Start</button>
+                    <button onClick={pauseTimer} disabled={controls.pause} className={`m-[0.4rem] px-[1rem] py-[0.4rem] text-[1.2rem] font-[500] border-none outline-none cursor-pointer rounded-[0.2rem] transition-all ease-in-out duration-300 ${controls.pause ? 'opacity-40' : 'opacity-100'} bg-[crimson]`} style={{fontFamily: 'Rethink Sans, sans-serif'}}>Pause</button>
+                    <button onClick={resetTimer} disabled={controls.reset} className={`m-[0.4rem] px-[1rem] py-[0.4rem] text-[1.2rem] font-[500] border-none outline-none cursor-pointer rounded-[0.2rem] transition-all ease-in-out duration-300 ${controls.reset ? 'opacity-40' : 'opacity-100'} bg-[steelblue]`} style={{fontFamily: 'Rethink Sans, sans-serif'}}>Reset</button>
                 </div>
             </div>
-            <Link to="/" className="fixed bottom-[3rem] left-[5%] text-[#555] text-[1.2rem] font-[500] underline">Home</Link>
+            <Link to="/" className="fixed bottom-[3rem] left-[5%] text-[#aaa] text-[1.2rem] font-[500] underline">Home</Link>
         </div>
     );
 }
