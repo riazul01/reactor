ae792809a271f29165319f2f85e07f53b37c3f30
Riazul Islam <riazul5207@gmail.com>
2024-05-22 09:33:35 +0600

    clock project updated

--- src/pages/projects/Clock.jsx
+++ src/pages/projects/Clock.jsx
@@ -1,60 +1,82 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Clock = () => {
-    const [time, setTime] = useState('00:00:00 XM');
-    const [date, setDate] = useState('day, month 00, 0000');
+  const [time, setTime] = useState("00:00:00 XM");
+  const [date, setDate] = useState("day, month 00, 0000");
 
-    useEffect(() => {
-        let timer = setInterval(() => {
+  useEffect(() => {
+    let timer = setInterval(() => {
+      const dt = new Date();
+      const dayNames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
 
-            const dt = new Date();
+      // Time
+      let hour = dt.getHours();
+      let minute = dt.getMinutes();
+      let second = dt.getSeconds();
 
-            const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      let xm = hour >= 12 ? "PM" : "AM";
 
-            // Time
-            let hour = dt.getHours();
-            let minute = dt.getMinutes();
-            let second = dt.getSeconds();
+      hour = hour > 12 ? hour - 12 : hour;
+      hour = hour === 0 ? (hour = 12) : hour;
 
-            let xm = (hour >= 12) ? 'PM' : 'AM';
+      let newTime = `${hour.toString().padStart(2, "0")}:${minute
+        .toString()
+        .padStart(2, "0")}:${second.toString().padStart(2, "0")} ${xm}`;
 
-            hour = (hour > 12) ? hour - 12 : hour;
-            hour = (hour === 0) ? hour = 12 : hour;
-            hour = (hour < 10) ? '0' + hour : hour;
-            minute = (minute < 10) ? '0' + minute : minute;
-            second = (second < 10) ? '0' + second : second;
+      setTime(newTime);
 
-            let newTime = `${hour}:${minute}:${second} ${xm}`;
-            setTime(newTime);
+      // Date
+      let dayIndex = dt.getDay();
+      let dayName = dayNames[dayIndex];
 
-            // Date
-            let dayIndex = dt.getDay();
-            let dayName = dayNames[dayIndex];
+      let monthIndex = dt.getMonth();
+      let monthName = monthNames[monthIndex];
 
-            let monthIndex = dt.getMonth();
-            let monthName = monthNames[monthIndex];
+      let today = dt.getDate();
+      let year = dt.getFullYear();
 
-            let today = dt.getDate();
-            let year = dt.getFullYear();
+      let newDate = `${dayName}, ${monthName} ${today
+        .toString()
+        .padStart(2, "0")}, ${year}`;
 
-            today = (today < 10) ? '0' + today : today;
+      setDate(newDate);
+    }, 1000);
 
-            let newDate = `${dayName}, ${monthName} ${today}, ${year}`;
-            setDate(newDate);
+    return () => clearInterval(timer);
+  }, [time, date]);
 
-        }, 1000);
-
-        return () => clearInterval(timer);
-
-    }, [time, date]);
-
-    return (
-        <div className="mx-auto font-mono flex flex-col items-center justify-center gap-[1rem] h-[100vh] w-full clock-bg bg-cover bg-fixed">
-            <h1 className="text-[#fff] text-[5rem] text-center font-[300] drop-shadow-lg">{time}</h1>
-            <p className="text-[#fff] text-[2.4rem] font-[300] text-center drop-shadow-lg">{date}</p>
-        </div>
-    );
-}
+  return (
+    <div className="mx-auto font-mono flex flex-col items-center justify-center gap-4 h-screen w-full bg-clock bg-cover bg-fixed">
+      <h1 className="text-white text-[5rem] text-center font-light drop-shadow-lg">
+        {time}
+      </h1>
+      <p className="text-white text-[2.4rem] text-center font-light drop-shadow-lg">
+        {date}
+      </p>
+    </div>
+  );
+};
 
 export default Clock;
