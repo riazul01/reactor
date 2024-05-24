03d9e9296b42d117551745b8e19015f83dec9e3e
Riazul Islam <riazul5207@gmail.com>
2024-05-24 21:53:03 +0600

    multiplication table project updated

--- src/pages/projects/multiply-table-generator/index.jsx
+++ src/pages/projects/multiply-table-generator/index.jsx
@@ -47,9 +47,9 @@ const MultiplyTableGenerator = () => {
   };
 
   return (
-    <div className="bg-table h-auto min-h-screen w-full bg-cover bg-fixed px-2 py-12 font-albert">
+    <div className="bg-table h-auto min-h-screen w-full overflow-x-hidden bg-cover bg-fixed px-2 py-12 font-albert">
       <div className="mx-auto w-full max-w-[1420px]">
-        <div className="width-full flex items-center justify-between">
+        <div className="width-full flex flex-col items-center justify-center gap-4 sm:flex-row sm:justify-between">
           <div className="flex items-center overflow-hidden rounded-lg outline-none">
             <p className="flex h-11 items-center justify-center bg-sky-600 px-5 text-xl font-medium text-white">
               Range
@@ -66,7 +66,7 @@ const MultiplyTableGenerator = () => {
           </p>
         </div>
 
-        <div className="mt-8 grid grid-cols-5 gap-4">
+        <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {Array.from(
             { length: range[1] - range[0] + 1 },
             (_, i) => i + range[0],
