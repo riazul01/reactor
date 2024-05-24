be8594f60a0b9a61b97c6ad77b38c07d1c9d03f8
Riazul Islam <riazul5207@gmail.com>
2024-05-24 21:08:35 +0600

    multiplication table project updated

--- src/pages/projects/multiply-table-generator/MultiplyTable.jsx
+++ src/pages/projects/multiply-table-generator/MultiplyTable.jsx
@@ -1,28 +1,41 @@
-import React, { useRef } from 'react';
-import { colors, colorsShades } from './colors';
+import React, { useRef } from "react";
+import { colors, colorsShades } from "./colors";
 
 const MultiplyTable = ({ tableNo }) => {
-    const table = useRef([]);
+  const table = useRef([]);
 
-    const colorIndex = Math.floor(Math.random() * colors.length);
-    const colorName = colors[colorIndex];
-    const colorMain = colorsShades[colorName][700];
-    const colorDark = colorsShades[colorName][800];
-    const colorDarker = colorsShades[colorName][900];
+  const colorIndex = Math.floor(Math.random() * colors.length);
+  const colorName = colors[colorIndex];
+  const colorMain = colorsShades[colorName][700];
+  const colorDark = colorsShades[colorName][800];
+  const colorDarker = colorsShades[colorName][900];
 
-    for (let i = 1; i <= 10; i ++) {
-        let line = `${tableNo} X ${i} = ${tableNo * i}`;
-        table.current[i - 1] = line;
-    }
+  for (let i = 1; i <= 10; i++) {
+    let line = `${tableNo} X ${i} = ${tableNo * i}`;
+    table.current[i - 1] = line;
+  }
 
-    return (
-        <div className="w-full rounded-lg overflow-hidden">
-            <h1 className="px-[1rem] py-[0.3rem] text-[1.4rem] text-[#fff] font-bold width-full" style={{background: colorMain}}>x{tableNo}</h1>
-            {table.current.map((line, index) => {
-                return <p key={index} className="px-[1rem] py-[0.4rem] text-[1.2rem] text-[#fff]" style={{background: (index % 2) ? colorDark : colorDarker}}>{line}</p>
-            })}
-        </div>
-    );
-}
+  return (
+    <div className="w-full overflow-hidden rounded-lg">
+      <h1
+        className="width-full px-4 py-2 text-2xl font-bold text-white"
+        style={{ background: colorMain }}
+      >
+        x{tableNo}
+      </h1>
+      {table.current.map((line, index) => {
+        return (
+          <p
+            key={index}
+            className="px-4 py-2 text-xl text-white"
+            style={{ background: index % 2 ? colorDark : colorDarker }}
+          >
+            {line}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
 
 export default MultiplyTable;
--- src/pages/projects/multiply-table-generator/index.jsx
+++ src/pages/projects/multiply-table-generator/index.jsx
@@ -1,62 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import MultiplyTable from './MultiplyTable';
+import React, { useState, useEffect } from "react";
+import MultiplyTable from "./MultiplyTable";
 
 const MultiplyTableGenerator = () => {
-    const [rangeInput, setRangeInput] = useState('1-20');
-    const [range, setRange] = useState([]);
+  const [rangeInput, setRangeInput] = useState("1-20");
+  const [range, setRange] = useState([]);
 
-    useEffect(() => {
-        const tempArr = rangeInput.split('-').filter((ch) => {
-            return !isNaN(parseInt(ch)) ? ch : null;
-        });
+  useEffect(() => {
+    const tempArr = rangeInput.split("-").filter((ch) => {
+      return !isNaN(parseInt(ch)) ? ch : null;
+    });
 
-        let min = parseInt(Math.min(...tempArr));
-        let max = parseInt(Math.max(...tempArr));
+    let min = parseInt(Math.min(...tempArr));
+    let max = parseInt(Math.max(...tempArr));
 
-        if (min !== max) {
-            min = parseInt(Math.min(...tempArr).toString().slice(0, 4));
-            max = parseInt(Math.max(...tempArr).toString().slice(0, 4));
-        }
-
-        if (min > max) {
-            [min, max] = [max, min];
-        }
-
-        setRange([min, max]);
-    }, [rangeInput]);
-
-    const handleInputChange = (e) => {
-        let value = e.target.value;
-
-        setTimeout(() => {
-            if (value === '') {
-                setRangeInput('');
-            }
-            if (/^[0-9-]+$/.test(value)) {
-                setRangeInput(value);
-            }
-        }, 1200);
+    if (min !== max) {
+      min = parseInt(
+        Math.min(...tempArr)
+          .toString()
+          .slice(0, 4),
+      );
+      max = parseInt(
+        Math.max(...tempArr)
+          .toString()
+          .slice(0, 4),
+      );
     }
 
-    return (
-        <div className="px-[0.4rem] py-[3rem] font-albert w-full h-auto min-h-[100vh] bg-table bg-cover bg-fixed">
-            <div className="mx-auto w-full max-w-[1420px]">
-                <div className="width-full flex items-center justify-between">
-                    <div className="flex items-center outline-none rounded-lg overflow-hidden">
-                        <p className="px-[1.2rem] text-[#fff] text-[1.2rem] flex items-center justify-center h-[44px] font-[500] bg-sky-600">Range</p>
-                        <input onChange={handleInputChange} name="limit" className="px-[0.6rem] h-[44px] w-[160px] text-[#fff] text-[1.2rem] bg-transparent border-[2px] border-sky-600 rounded-r-lg outline-none" placeholder={rangeInput === "1-20" ? "1-20" : "![range]"}/>
-                    </div>
-                    <p className="text-[#aaa] text-[1rem] italic">Multiplication table from {range[0]} to {range[1]}</p>
-                </div>
+    if (min > max) {
+      [min, max] = [max, min];
+    }
 
-                <div className="mt-[2rem] grid grid-cols-5 gap-[1rem]">
-                    {Array.from({ length: range[1] - range[0] + 1 }, (_, i) => i + range[0]).map((tableNo, index) => {
-                        return <MultiplyTable key={index} tableNo={tableNo}/>
-                    })}
-                </div>
-            </div>
+    setRange([min, max]);
+  }, [rangeInput]);
+
+  const handleInputChange = (e) => {
+    let value = e.target.value;
+
+    setTimeout(() => {
+      if (value === "") {
+        setRangeInput("");
+      }
+      if (/^[0-9-]+$/.test(value)) {
+        setRangeInput(value);
+      }
+    }, 1200);
+  };
+
+  return (
+    <div className="bg-table h-auto min-h-screen w-full bg-cover bg-fixed px-2 py-12 font-albert">
+      <div className="mx-auto w-full max-w-[1420px]">
+        <div className="width-full flex items-center justify-between">
+          <div className="flex items-center overflow-hidden rounded-lg outline-none">
+            <p className="flex h-11 items-center justify-center bg-sky-600 px-5 text-xl font-medium text-white">
+              Range
+            </p>
+            <input
+              onChange={handleInputChange}
+              name="limit"
+              className="h-11 w-40 rounded-r-lg border-2 border-sky-600 bg-transparent px-3 text-xl text-white outline-none"
+              placeholder={rangeInput === "1-20" ? "1-20" : "![range]"}
+            />
+          </div>
+          <p className="text-base italic text-neutral-400">
+            Multiplication table from {range[0]} to {range[1]}
+          </p>
         </div>
-    );
-}
+
+        <div className="mt-8 grid grid-cols-5 gap-4">
+          {Array.from(
+            { length: range[1] - range[0] + 1 },
+            (_, i) => i + range[0],
+          ).map((tableNo, index) => {
+            return <MultiplyTable key={index} tableNo={tableNo} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MultiplyTableGenerator;
