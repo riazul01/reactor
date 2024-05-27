37da4db3ae759eb9f7ea88c7a12811e7849ca4f8
Riazul Islam <riazul5207@gmail.com>
2024-05-27 18:49:09 +0600

    footer ui updated

--- src/pages/home/Footer.jsx
+++ src/pages/home/Footer.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="mx-auto px-2 py-8 max-w-[1300px] flex items-center justify-between">
-            <p className="text-[#333] text-[0.9rem] text-shadow">Created by <a href="https://github.com/riazul01" target="_blank" rel="noreferrer" className="text-[#adff2f4d]">riazul01</a> | &copy; 2024 all rights reserved</p>
-            <div class="flex items-center justify-center gap-[0.6rem]">
-                <a href="https://github.com/riazul01" target="_blank" rel="noreferrer" className="text-[#333] text-[0.9rem]" style={{textShadow: '0.1rem 0.1rem 0.2rem #000'}}>GitHub</a>
-                <span className="h-[0.2rem] w-[0.2rem] rounded-full bg-[#222]"></span>
-                <a href="https://www.linkedin.com/in/md-riazul-islam-86653321b/" target="_blank" rel="noreferrer" className="text-[#333] text-[0.9rem]" style={{textShadow: '0.1rem 0.1rem 0.2rem #000'}}>LinkedIn</a>
-                <span className="h-[0.2rem] w-[0.2rem] rounded-full bg-[#222]"></span>
-                <a href="https://riazul.vercel.app/" target="_blank" rel="noreferrer" className="text-[#333] text-[0.9rem]" style={{textShadow: '0.1rem 0.1rem 0.2rem #000'}}>Portfolio</a>
-            </div>
-        </footer>
-    );
-}
+  return (
+    <footer className="mx-auto flex max-w-[1300px] flex-col items-center justify-between gap-3 px-2 py-8 sm:flex-row sm:gap-0">
+      <p className="text-sm text-[#333] text-shadow">
+        Created by{" "}
+        <a
+          href="https://github.com/riazul01"
+          target="_blank"
+          rel="noreferrer"
+          className="text-[#adff2f4d] underline"
+        >
+          riazul01
+        </a>{" "}
+        | &copy; 2024 all rights reserved
+      </p>
+      <div class="flex items-center justify-center gap-2 sm:gap-2.5">
+        <a
+          href="https://github.com/riazul01"
+          target="_blank"
+          rel="noreferrer"
+          className="text-sm text-[#333] text-shadow"
+        >
+          GitHub
+        </a>
+        <span className="h-[0.2rem] w-[0.2rem] rounded-full bg-[#222]"></span>
+        <a
+          href="https://www.linkedin.com/in/md-riazul-islam-86653321b/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-sm text-[#333] text-shadow"
+        >
+          LinkedIn
+        </a>
+        <span className="h-[0.2rem] w-[0.2rem] rounded-full bg-[#222]"></span>
+        <a
+          href="https://riazul.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-sm text-[#333] text-shadow"
+        >
+          Portfolio
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
