b82e61ffdef8d61023fed91072aa8c959033dfd9
Riazul Islam <riazul5207@gmail.com>
2024-05-28 23:19:11 +0600

    cleaned up

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Clock from "./pages/projects/Clock";
 import Stopwatch from "./pages/projects/Stopwatch";
-import SplitScreen from "./pages/projects/split-screen";
-import Counter from "./pages/projects/counter";
 import Todo from "./pages/projects/Todo";
+import Counter from "./pages/projects/counter";
 import RandomColors from "./pages/projects/random-colors";
 import PxToREM from "./pages/projects/PxToREM";
 import AgeCalculator from "./pages/projects/AgeCalculator";
 import PasswordGenerator from "./pages/projects/password-generator";
+import Algorithms from "./pages/projects/algorithms";
 import RotationCounter from "./pages/projects/RotationCounter";
 import MultiplicationTableGenerator from "./pages/projects/multiplication-table-generator";
-import Algorithms from "./pages/projects/algorithms";
+import SplitScreen from "./pages/projects/split-screen";
 
 const App = () => {
   return (
@@ -23,22 +23,22 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/clock" element={<Clock />} />
         <Route path="/stopwatch" element={<Stopwatch />} />
-        <Route path="/split-screen" element={<SplitScreen />} />
         <Route path="/todo" element={<Todo />} />
         <Route path="/counter" element={<Counter />} />
         <Route path="/random-colors" element={<RandomColors />} />
         <Route path="/px-to-rem" element={<PxToREM />} />
         <Route path="/age-calculator" element={<AgeCalculator />} />
-        <Route path="/rotation-counter" element={<RotationCounter />} />
         <Route
           path="/random-password-generator"
           element={<PasswordGenerator />}
         />
+        <Route path="/algorithms" element={<Algorithms />} />
+        <Route path="/rotation-counter" element={<RotationCounter />} />
         <Route
           path="/multiplication-table-generator"
           element={<MultiplicationTableGenerator />}
         />
-        <Route path="/algorithms" element={<Algorithms />} />
+        <Route path="/split-screen" element={<SplitScreen />} />
       </Routes>
     </BrowserRouter>
   );
